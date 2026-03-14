75b18f03ef9e4921c97a2e147ab46d284e0c73ee
John Aven <john.aven.phd@gmail.com>
2026-03-14 00:29:41 -0500

    fix: resolve import errors in test files by replacing incorrect REPO_TYPE_* imports with RepositoryTypes class references

--- promptosaurus/questions/base/constants.py
+++ promptosaurus/questions/base/constants.py
@@ -1,12 +1,14 @@
 """Repository types and configuration values."""
 
+
 class RepositoryTypes:
     SINGLE = "single-language"
     MULTI_MONOREPO = "multi-language-monorepo"
     MIXED = "mixed"
-    
+
     @classmethod
     def all(cls):
         return [cls.SINGLE, cls.MULTI_MONOREPO, cls.MIXED]
+
 
 REPO_TYPES = RepositoryTypes.all()
