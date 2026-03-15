78351259def898e51bcdb70bf753db18fac79b0c
John Aven <john.aven.phd@gmail.com>
2026-03-15 00:05:17 -0500

    fix: added verison information back in

--- promptosaurus/__init__.py
+++ promptosaurus/__init__.py
@@ -2,5 +2,12 @@
 
 from sweet_tea.registry import Registry
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("promptosaurus")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 # sweet_tea auto-registers all imported classes
 Registry.fill_registry(library="promptosaurus")
